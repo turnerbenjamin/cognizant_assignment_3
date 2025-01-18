f5ba1b597fd219715aa0ae4432744323582b7cce
turnerbenjamin <turner.benjamin88@gmail.com>
2025-01-18 14:15:34 +0000

    REFACTOR: refactored following review

--- s2_plugin/SingleActiveCaseEnforcement/Model/Case.cs
+++ s2_plugin/SingleActiveCaseEnforcement/Model/Case.cs
@@ -14,21 +14,21 @@ namespace SingleActiveCaseEnforcement.Model
         public EntityReference Customer { get; }
         public OptionSetValue Status { get; }
 
+        /// <summary>
+        ///     Initialises a new instance of the <see cref="Case"/> class.
+        /// </summary>
+        /// <param name="caseEntity">The entity representing the case.</param>
+        /// <exception cref="ArgumentNullException">
+        ///     Thrown when the case entity is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        ///     Thrown when the case entity does not have the correct logical
+        ///     name.
+        /// </exception>
         public Case(Entity caseEntity)
         {
-            if (caseEntity is null)
-            {
-                throw new ArgumentNullException(
-                    $"{nameof(caseEntity)} must not be null"
-                );
-            }
-
-            if (caseEntity.LogicalName != EntityLogicalName)
-            {
-                throw new ArgumentException(
-                    $"Target must be of type {EntityLogicalName}"
-                );
-            }
+            GuardEntityIsNotNull(caseEntity);
+            GuardEntityHasTheCorrectType(caseEntity);
 
             Title = caseEntity.GetAttributeValue<string>(TitleFieldLogicalName);
             Customer = caseEntity.GetAttributeValue<EntityReference>(
@@ -37,6 +37,58 @@ namespace SingleActiveCaseEnforcement.Model
             Status = caseEntity.GetAttributeValue<OptionSetValue>(
                 StatusFieldLogicalName
             );
+        }
+
+        /// <summary>
+        ///     Explicit casting of an entity to a case
+        /// </summary>
+        /// <param name="caseEntity">The entity representing the case.</param>
+        /// <exception cref="ArgumentNullException">
+        ///     Thrown when the case entity is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        ///     Thrown when the case entity does not have the correct logical
+        ///     name.
+        /// </exception>
+        public static explicit operator Case(Entity caseEntity)
+        {
+            return new Case(caseEntity);
+        }
+
+        /// <summary>
+        ///     Ensures the entity is not null.
+        /// </summary>
+        /// <param name="entity">The entity to check.</param>
+        /// <exception cref="ArgumentNullException">
+        ///     Thrown when the entity is null.
+        /// </exception>
+        private void GuardEntityIsNotNull(Entity entity)
+        {
+            if (entity is null)
+            {
+                throw new ArgumentNullException(
+                    nameof(entity),
+                    "Entity must not be null"
+                );
+            }
+        }
+
+        /// <summary>
+        /// Ensures the entity has the correct logical name.
+        /// </summary>
+        /// <param name="entity">The entity to check.</param>
+        /// <exception cref="ArgumentException">
+        ///     Thrown when the entity does not have the correct logical name.
+        /// </exception>
+        private void GuardEntityHasTheCorrectType(Entity entity)
+        {
+            if (entity.LogicalName != EntityLogicalName)
+            {
+                throw new ArgumentException(
+                    $"Entity must be of type {EntityLogicalName}",
+                    nameof(entity)
+                );
+            }
         }
     }
 }
